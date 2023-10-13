aab13b7f6cf8bf71792514d696a2d9ec291c5b1d
kyleuniversities <kyleuniversities@gmail.com>
2023-10-13 15:47:50 -0700

    reestablished as with browser router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './app/home/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
