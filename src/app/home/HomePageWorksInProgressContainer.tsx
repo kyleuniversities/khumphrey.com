245d6af00c0e95ed5504bd1b26bcd7e924839425
kyleuniversities <kyleuniversities@gmail.com>
2023-10-14 18:17:43 -0700

    added works in progress section

--- src/app/home/HomePageWorksInProgressContainer.tsx
+++ src/app/home/HomePageWorksInProgressContainer.tsx
@@ -1,7 +1,25 @@
+import { WorkInProgressCard } from '../project/WorkInProgressCard';
+import { HomePageTransitionContainer } from './HomePage';
+
 export const HomePageWorksInProgressContainer = () => {
   return (
-    <div>
-      <p>Works in Progress</p>
-    </div>
+    <HomePageTransitionContainer
+      height="480px"
+      animation="slide left"
+      timeout={3500}
+    >
+      <div
+        style={{
+          textAlign: 'left',
+          marginLeft: '100px',
+          marginRight: '100px',
+        }}
+      >
+        <span style={{ fontSize: '34px', fontWeight: 'bold' }}>
+          <p>Works in Progress</p>
+        </span>
+        <WorkInProgressCard name="full-stack-certificate" />
+      </div>
+    </HomePageTransitionContainer>
   );
 };
