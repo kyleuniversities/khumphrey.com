ac17d23677284c9f92a58c55930fe3f80a9602aa
kyleuniversities <kyleuniversities@gmail.com>
2023-11-04 00:36:59 -0700

    added GridRowMultilineBreak

--- src/common/util/js/line.tsx
+++ src/common/util/js/line.tsx
@@ -1,7 +1,19 @@
+import { Grid } from 'semantic-ui-react';
+
 export const MultiLineBreak = (props: { lines: number }): JSX.Element => {
   const breakList = [];
   for (let i = 0; i < props.lines; i++) {
     breakList.push(<br></br>);
   }
   return <>{breakList}</>;
+};
+
+export const GridRowMultiLineBreak = (props: {
+  lines: number;
+}): JSX.Element => {
+  return (
+    <Grid.Row>
+      <MultiLineBreak lines={props.lines} />
+    </Grid.Row>
+  );
 };
