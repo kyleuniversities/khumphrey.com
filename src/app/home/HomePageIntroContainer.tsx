048a0bb9a6d12bbef115c9dd32263d6fb43169af
kyleuniversities <kyleuniversities@gmail.com>
2023-11-10 02:31:18 -0800

    added quick welcome resource fetching

--- src/app/home/HomePageIntroContainer.tsx
+++ src/app/home/HomePageIntroContainer.tsx
@@ -7,7 +7,7 @@ import {
   STATIC_ANIMATION,
 } from './HomePage';
 import './index.css';
-import { getResourceUrl } from '../util/resource';
+import { getQuickResourceUrl } from '../util/resource';
 
 /**
  * Home Page component for introductory content
@@ -25,7 +25,7 @@ export const HomePageIntroContainer = (props: AnimationSwitch): JSX.Element => {
  * Container for the welcome image
  */
 const HomePageIntroWelcomeContainer = (props: AnimationSwitch): JSX.Element => {
-  const welcome = getResourceUrl('resources/welcome.png');
+  const welcome = getQuickResourceUrl('resources/welcome.png');
   return (
     <HomePageTransitionContainer
       className="homeWelcomeTransitionSection"
