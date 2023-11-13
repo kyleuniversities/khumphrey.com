2548d931832bd3b22e1a26ca78e6c346e97b698b
kyleuniversities <kyleuniversities@gmail.com>
2023-11-13 00:25:04 -0800

    add project link text wrapping

--- src/app/ResourcePage.tsx
+++ src/app/ResourcePage.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { useMediaQuery } from 'react-responsive';
 import { SitePage } from './SitePage';
 import { Container } from 'semantic-ui-react';
 import { MultiLineBreak } from '../common/util/js/line';
@@ -7,6 +8,9 @@ import ReactMarkdown from 'react-markdown';
 import { fetchText } from './util/fetch';
 import './index.css';
 import { getResourceUrl } from './util/resource';
+import { BIG_SCREEN_QUERY, MEDIUM_SCREEN_QUERY } from '../common/util/mobile';
+import { ifThen } from '../common/util/conditional';
+import { MarkdownHelper } from './helper/MarkdownHelper';
 
 /**
  * Page that loads a resource
@@ -32,9 +36,17 @@ export const ResourceTextPage = (props: { dataToken: string }): JSX.Element => {
   const resourceUrl = getResourceUrl(
     `resources/${props.dataToken}/${id}/full.md`
   );
+  const isBigScreen = useMediaQuery(BIG_SCREEN_QUERY);
+  const isMediumScreen = useMediaQuery(MEDIUM_SCREEN_QUERY);
+  const isSmallScreen = !isBigScreen && !isMediumScreen;
   useEffect(() => {
-    fetchText(resourceUrl, setResourcePageText);
-  }, [resourceUrl]);
+    fetchText(resourceUrl, (text) => {
+      ifThen(isSmallScreen, () =>
+        setResourcePageText(MarkdownHelper.reformat(text, 40))
+      );
+      ifThen(!isSmallScreen, () => setResourcePageText(text));
+    });
+  }, [resourceUrl, isSmallScreen]);
   return (
     <ResourcePage>
       <ReactMarkdown className="reactMarkdown" children={resourcePageText} />
